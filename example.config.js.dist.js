38d74791e27c3f05328fdc0542298867d5502991
gordywills <gordy.wills@gmail.com>
2017-02-13 23:10:19 +0000

    add example config

---
+++ example.config.js.dist.js
@@ -0,0 +1,13 @@
+
+var config = {
+    modules: {
+        module: "MMM-MyTemps",
+        header: "Is it cold?",
+        position: "top_left",
+        config: {
+            host: "YOUR HOST GOES HERE", //eg "192.168.1.2" or "www.mydomain.com"
+            path: "PATH GOES HERE", //eg "/sensors/temp2cf.php?format=json"
+            reloadInterval: 60 * 1000 // 1 min - time between reloads of data
+        }
+    }
+};
